a7397d34425f1de7d3f43f3400666dfc4f9380c1
nistmathman <bis@nist.gov>
2019-03-10 10:00:55 -0400

    Again

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -6,8 +6,9 @@
       <hr/>
   <img class="img-responsive" src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/AMPGatewayThemes.png" width="100%" height="100%"/>
 
-    <video controls class="video-responsive" src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/movie.mp4" width="40%" height="40%">
+
 <h1> <center> H_2 Molecular Ion in Strong, Ultrafast Electromagnetic Field </center> </h1>
+ <video controls class="video-responsive" src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/movie.mp4" width="40%" height="40%">
   <!--
     <h1>Hero Image</h1>
     <h2>This is a simple hero unit, a simple jumbotron-style component for calling extra 7
