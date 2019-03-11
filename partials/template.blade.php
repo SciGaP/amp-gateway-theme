33fa59a3afb3ae26d0c447f684ea3ce45403b797
spamidig <pamidigs@iu.edu>
2019-03-11 13:15:30 -0400

    updated

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -5,6 +5,7 @@
       </p>
       <hr/>
   <img class="img-responsive" src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/AMPGatewayThemes_pvs4.png" width="100%" height="100%"/>
+      <p style="color:#cccccc;">
 <h2> <center> <font color="red"> H<sup>+</sup><sub>2</sub> Molecular Ion in Strong, Ultrafast Electromagnetic Field </center> </h2>
  <center> <video controls class="video-responsive" src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/movie.mp4" width="40%" height="40%"> <\center>
 <font color="black"><\font>
