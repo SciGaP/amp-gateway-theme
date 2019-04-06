4c863a8cc3135fed554bf21ab0a51c9b565f344f
Schneider <bis@bismac.local>
2019-04-06 11:56:51 -0400

    OK

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -6,7 +6,7 @@
     <p style="font-size:18px; margin-top:20px;" align="left">
      Please refer to
      <p> <a href="http://itp.curtin.edu.au/research/">Convergent Close-Coupling (CCC) method </a> </p>
-     <p> <a href="https://github.com/jesusG-V/xchemQC">XChem: The ab initio Solution for Multichannel Scattering Problems </a></p>
+     <p> <a href="https://www.xchem.uam.es/xchem/?tag=xchem">XChem: The ab initio Solution for Multichannel Scattering Problems </a></p>
      <p> <a href="https://trecx.physik.lmu.de/home.html"> tRecX: time-dependent Recursive indeXing </a> </p>
      <p> <a href="http://cpc.cs.qub.ac.uk/summaries/ADWY"> Atomic continuum processes using the B-spline R-matrix method </a> </p>
      <p> <a href="https://drive.google.com/open?id=0B6dM3u6iD10RMGRYTHI4TEVLNW8"> MESA: Electronic structure and scattering </a> </p>
