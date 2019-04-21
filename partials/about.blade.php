3236e2d77513efc4fdb9f235651c403130b4412b
Schneider <bis@bismac.local>
2019-04-21 08:54:56 -0400

    OK

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -5,20 +5,29 @@
   <p style="font-size:18px; margin-top:20px;" align="left">
 The scientists involved in this project have been active researchers in computational atomic and molecular physics for many years and have been ably assisted by Dr Sudhakar Pamidighantam, a theoretical chemist by training, who is part of the NSF XSEDE ECSS team.  The members are;
 <ul style="font-size:18px">
- <li> Dr Barry I. Schneider, Applied and Computational Mathematics Division, National Instoitute of Standards and Technology, Gaithersburg, MD 20899, USA </li>
+ <li> Dr Barry I. Schneider, Applied and Computational Mathematics Division, National Institute of Standards and Technology, Gaithersburg, MD 20899, USA </li>
  <li> Prof. Klaus Bartschat, Dept. of Physics and Astronomy, Drake University, Des Moines, IA, 50311, USA </li>
 <li> Dr. Oleg Zatsarinny, Dept. of Physics and Astronomy, Drake University, Des Moines, IA, 50311, USA </li>
- <li> Prof. Igor Bray, Dept. of Physics and Astronomy, Curtin University, Perth, Western Australia  ,6845 </li>
+ <li> Prof. Igor Bray, Dept. of Physics and Astronomy, Curtin University, Perth, Western Australia  , 6845 </li>
  <li> Prof. Fernando Martin, Dept. of Chemistry, Universidad Autónoma de Madrid, Cantoblanco 28049 Madrid, Spain </li>
 <li> Dr. Markus Klinker, Dept. of Chemistry, Universidad Autónoma de Madrid, Cantoblanco 28049 Madrid, Spain </li>
 <li> Prof. Armin Scrinzi, Faculty of Physics, Ludwig-Maximilians Universität, München, Germany </li>
 <li> Prof. Jonathan Tennyson, Dept. of Physics, University College London, Gower Street, London, WC1E 6BT, UK </li>
 <li> Prof. Jimena D. Gorfinkiel, Faculty of Science,Technology, Engineering and Mathematics, Open University, Milton Keynes MK7 6AA, UK</li>
  <hr class="separation-line blue-bg"/>
-  <h2 class="text-center">Training and Documentation</h2>
+  <h2 class="text-center">Our Motivation and Objectives</h2>
     <hr class="separation-line blue-bg"/>
 
     <p style="font-size:18px; margin-top:20px;" align="left">
+     The gateway was a result of a workshop at the Instittute for Theoretical Atomic and Molecular
+     Physics (ITAMP) held at the Harvard-Smithsonian in May of 018.   A subset of the attendees
+     felt the time was ripe to bring together the AMP community to work collectively to make
+     their codes available and easier to use by the partners as well as the AMP community at
+     large. By necessity, a project such as this requires the developers to work on issues of
+     portability, documentation, ease of input,as well as making sure the codes can run on a
+     variety of architectures.
+
+     
       AMPGateway is an online portal that hosts a suite of codes being developed by the atomic and molecular physics 
       community to study a variety of problems involving the time-independent and time-dependent Schrödinger equation. 
       They include the calculation of cross sections for electron scattering and photoionization from atoms and molecules
